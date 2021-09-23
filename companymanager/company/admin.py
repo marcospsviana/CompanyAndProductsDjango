02f817b3aecef27999b49a47098e50583d4a265e
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-09-22 22:55:18 -0300

    feito migrate

--- companymanager/company/admin.py
+++ companymanager/company/admin.py
@@ -1,3 +1,17 @@
-# from django.contrib import admin
+from django.contrib import admin
+from companymanager.company.models import Company, Product, ListProducts
 
 # Register your models here.
+@admin.register(Company)
+class AdminCompany(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ["name"]
+
+
+@admin.register(ListProducts)
+class AdminListProduct(admin.ModelAdmin):
+    list_display = ["company", "product"]
--- companymanager/company/models.py
+++ companymanager/company/models.py
@@ -4,9 +4,18 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=60)
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "companies"
+
 
 class Product(models.Model):
     name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
 
 
 class ListProducts(models.Model):
