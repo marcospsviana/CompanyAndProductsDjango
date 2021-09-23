c493a7a3b41e03b7fdee7dec08ac9136781e536f
marcos p s viana <marcospaulo.silvaviana@gmail.com>
2021-09-22 23:01:47 -0300

    feito migrate (#4)

--- companymanager/company/models.py
+++ companymanager/company/models.py
@@ -1,3 +1,27 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "companies"
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+
+class ListProducts(models.Model):
+    company = models.ForeignKey(
+        Company, related_name="company", on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product, related_name="product", on_delete=models.CASCADE
+    )
