1aed69319f2e31103e98bfdd892237e1b79f0a0b
marcospsviana <marcospaulo.silvaviana@gmail.com>
2021-09-22 22:06:14 -0300

    alterado imports nome diretorio do projeto

--- companymanager/company/models.py
+++ companymanager/company/models.py
@@ -1,9 +1,6 @@
 from django.db import models
 
 
-
-
-
 class Company(models.Model):
     name = models.CharField(max_length=60)
 
@@ -11,7 +8,11 @@ class Company(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=150)
 
-class ListProducts(models.Model):
-    company = models.ForeignKey(Company, related_name='company', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, related_name='product', on_delete=models.CASCADE)
 
+class ListProducts(models.Model):
+    company = models.ForeignKey(
+        Company, related_name="company", on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product, related_name="product", on_delete=models.CASCADE
+    )
